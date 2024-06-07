6ebd8f16f8dc050f9ff93d26a3380a253f367f17
sardorbeksidikov <sardorsidikov005@gmail.com>
2024-06-07 10:12:12 +0500

    secont comment

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
+    <div>
+      {" "}
+      <div>
+        <div className="carousel">
+          <button className="carousel-button prev" onClick={goToPrevious}>
+            ❮
+          </button>
+          <div className="carousel-images">
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className={`carousel-image ${
+                  index === currentIndex ? "active" : ""
+                }`}>
+                <img src={image} alt={`Slide ${index}`} />
+              </div>
+            ))}
+          </div>
+          <button className="carousel-button next" onClick={goToNext}>
+            ❯
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Shop
